087b177af7bb8a08123e994693afb75be3da14e5
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2023-12-12 11:04:00 -0300

    Add solution to problem 1464

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         int[] testArr = { 1, 2, 3, 3 };
 
         Solution1287 s = new Solution1287();
+        Solution1464 s2 = new Solution1464();
 
         System.out.println(s.findSpecialInteger(testArr));
+        System.out.println(s2.maxProduct(testArr));
     }
 }
+
