cee920becbd5aa74e89e28b8d8905909aa40187c
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2023-12-27 11:39:56 -0300

    Add optimized solution to problem 1

--- Solution1.java
+++ Solution1.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
@@ -15,6 +18,26 @@ class Solution1 {
         return arr;
     }
 
+    public int[] twoSumV2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int x = target - current;
+
+            if (map.containsKey(x)) {
+                arr[0] = i;
+                arr[1] = map.get(x);
+                break;
+            } else {
+                map.put(current, i);
+            }
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         Solution1 sol = new Solution1();
 
@@ -22,7 +45,10 @@ class Solution1 {
         int target = 9;
 
         int[] result = sol.twoSum(nums, target);
+        int[] resultV2 = sol.twoSumV2(nums, target);
+        
         System.out.println(result[0] + " " + result[1]);
+        System.out.println(resultV2[0] + " " + resultV2[1]);
     }
 }
 
